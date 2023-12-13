183e2a26cfee3893dee8a980b5c0ca818e60ffdb
sakai <sakai263016@gmail.com>
2023-12-13 16:09:00 +0800

    String练习：截取域名中的一部分

--- src/main/java/string/StringPractice.java
+++ src/main/java/string/StringPractice.java
@@ -4,17 +4,18 @@ public class StringPractice {
     public static void main(String[] args) {
         String url1 = "http://www.tedu.cn";
         String url2 = "www.tarena.com.cn";
+        //String url3 = "https://www.bard.google.com/chat";
         String hostName1 = getHostName(url1);
         String hostName2 = getHostName(url2);
+        //String hostName3 = getHostName(url3);
         System.out.println(hostName1);
         System.out.println(hostName2);
+        //System.out.println(hostName3);
     }
 
     public static String getHostName(String url) {
         int indexStar = url.indexOf(".")+1;
         int indexEnd = url.indexOf(".", indexStar);
-        String subUrl;
-        subUrl = url.substring(indexStar , indexEnd);
-        return subUrl;
+        return url.substring(indexStar, indexEnd);
     }
 }
