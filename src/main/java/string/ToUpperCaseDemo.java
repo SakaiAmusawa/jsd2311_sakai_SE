1025de19f7c77a4c686a5b3e2ba736242d8f315f
sakai <sakai263016@gmail.com>
2023-12-13 17:31:21 +0800

    String中toLowerCase()/toUpperCase()的基本概念与操作(将给定的字符串转换为小写/大写)

---
+++ src/main/java/string/ToUpperCaseDemo.java
@@ -0,0 +1,17 @@
+package string;
+
+/**
+ * toUpperCase();转换为大写
+ * toLowerCase();转换为小写
+ */
+public class ToUpperCaseDemo {
+    public static void main(String[] args) {
+        String name = "SakaiAmusawa";
+
+        String low = name.toLowerCase();
+        System.out.println(low);
+
+        String upper = name.toUpperCase();
+        System.out.println(upper);
+    }
+}
