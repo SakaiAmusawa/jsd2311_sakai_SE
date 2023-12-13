814ddafb225638395a32bb2c8266169c6c59fbb2
sakai <sakai263016@gmail.com>
2023-12-13 14:58:44 +0800

    String中length基本概念与操作

---
+++ src/main/java/string/StringLengthDemo.java
@@ -0,0 +1,10 @@
+package string;
+
+public class StringLengthDemo {
+    public static void main(String[] args) {
+        String str = "i love java";
+        int length = str.length();
+        System.out.println(length);
+
+    }
+}
