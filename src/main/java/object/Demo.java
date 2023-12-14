df0dacfcdc6fca2df0b5b4119648b208b2620939
sakai <sakai263016@gmail.com>
2023-12-14 16:01:00 +0800

    toString()方法的重写以及应用场景

---
+++ src/main/java/object/Demo.java
@@ -0,0 +1,15 @@
+package object;
+
+public class Demo {
+    public static void main(String[] args) {
+        Point p = new Point(1, 2);
+        String str = p.toString();
+        System.out.println(str);
+
+        //字符串连接时会将p自动转化为String(toString())
+        String name = "sakai" + p;
+        //想控制台输出一个对象时也是先将p转化为String类型后再输出
+        System.out.println(name);
+
+    }
+}
