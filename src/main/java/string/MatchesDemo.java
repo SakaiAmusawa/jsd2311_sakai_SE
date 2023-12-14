bd9960bf5432e50efbbea94c877cabc0a727d010
sakai <sakai263016@gmail.com>
2023-12-14 11:40:35 +0800

    正则表达式验证字符串

--- src/main/java/string/MatchesDemo.java
+++ src/main/java/string/MatchesDemo.java
@@ -13,6 +13,7 @@ public class MatchesDemo {
         说明：
         + 表示前面一个()/[]内的内容至少出现一次
          */
+        //切记不要乱加符号 比如空格
         String regex = "[a-zA-Z0-9_]+@[a-zA-Z0-9]+(\\.[a-zA-Z]+)+";
         String email = "sakai_Amusawa3016@gmail.com";
 
