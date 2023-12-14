ebd1c69444d8ef2c46b3d6ccdb3f664bceab29b0
sakai <sakai263016@gmail.com>
2023-12-14 17:06:09 +0800

    toString()方法的重写以及应用场景以及equals的方法的重写

--- src/main/java/object/Demo.java
+++ src/main/java/object/Demo.java
@@ -11,5 +11,9 @@ public class Demo {
         //想控制台输出一个对象时也是先将p转化为String类型后再输出
         System.out.println(name);
 
+
+        Point p2 = new Point(1, 2);
+        System.out.println(p == p2);
+        System.out.println(p.equals(p2));
     }
 }
