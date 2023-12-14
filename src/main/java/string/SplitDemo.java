dad16425d224188eae87a1f1067de35db6c93aa6
sakai <sakai263016@gmail.com>
2023-12-14 14:16:05 +0800

    String支持的正则表达式方法：.split(regex)

--- src/main/java/string/SplitDemo.java
+++ src/main/java/string/SplitDemo.java
@@ -16,5 +16,13 @@ public class SplitDemo {
         str = "a==b=c";
         strArr = str.split("=");
         System.out.println(Arrays.toString(strArr));
+        //字符串最开始就是可拆分项时，那么拆分出来的第一个就是空字符串
+        str = "==a=b=c";
+        strArr = str.split("=");
+        System.out.println(Arrays.toString(strArr));
+        //如果末尾出现可拆分项，那么可以省略
+        str = "a=b=c===";
+        strArr = str.split("=");
+        System.out.println(Arrays.toString(strArr));
     }
 }
