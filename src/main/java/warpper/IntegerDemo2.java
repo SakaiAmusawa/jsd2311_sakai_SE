6d9aa8c6e22f8e3921f73d3fb203d6bc984463f8
sakai <sakai263016@gmail.com>
2023-12-14 17:21:37 +0800

    Integer类的用法

---
+++ src/main/java/warpper/IntegerDemo2.java
@@ -0,0 +1,15 @@
+package warpper;
+
+public class IntegerDemo2 {
+    public static void main(String[] args) {
+        int iMax = Integer.MAX_VALUE;
+        System.out.println(iMax);
+        int iMin = Integer.MIN_VALUE;
+        System.out.println(iMin);
+
+        double dMax = Double.MAX_VALUE;
+        System.out.println(dMax);
+        double dMin = Double.MIN_VALUE;
+        System.out.println(dMin);
+    }
+}
