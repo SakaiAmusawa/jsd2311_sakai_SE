2b9026919697213825a3fefc52ae1f59e34fa6be
sakai <sakai263016@gmail.com>
2023-12-14 15:08:32 +0800

    String中replaceAll的基本操作与演示(替换)

---
+++ src/main/java/string/ReplaceDemo.java
@@ -0,0 +1,20 @@
+package string;
+
+import com.sun.org.apache.xpath.internal.operations.Number;
+
+public class ReplaceDemo {
+    public static void main(String[] args) {
+        String string = "abc123efd456ghi";
+/*        string = string.replaceAll("[0-9]+", "number");
+        System.out.println(string);*/
+
+        string = string.replaceAll("[a-z]+", "world");
+        System.out.println(string);
+
+        String regex = "(nc|jb|nm)";
+        String message = "你说nm个jb那，nc玩意";
+
+        message = message.replaceAll(regex, "***");
+        System.out.println(message);
+    }
+}
