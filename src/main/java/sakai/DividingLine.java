7722b3b5ec414da62cee57c9ba4d14e1d3fbe2c8
sakai <sakai263016@gmail.com>
2023-12-18 13:54:21 +0800

    DividingLine-sakai

---
+++ src/main/java/sakai/DividingLine.java
@@ -0,0 +1,11 @@
+package sakai;
+
+public class DividingLine {
+    public  static void dividingLine() {
+        System.out.println("----------------");
+    }
+
+    public static void dividingLine(String s) {
+        System.out.println("--------" + s + "--------");
+    }
+}
