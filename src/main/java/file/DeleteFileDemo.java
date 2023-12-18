b59d6b27d149cfa6d4b811819e196a4c5ee52108
sakai <sakai263016@gmail.com>
2023-12-18 17:01:07 +0800

    演示File的简单的操作

--- src/main/java/file/DeleteFileDemo.java
+++ src/main/java/file/DeleteFileDemo.java
@@ -5,7 +5,9 @@ import java.io.File;
 public class DeleteFileDemo {
     public static void main(String[] args) {
         File file = new File("./test.txt");
+        //判断文件是否存在
         if (file.exists()) {
+            //存在删除
             file.delete();
             System.out.println(file.getName() + " has deleted");
         } else {
