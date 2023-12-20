f43421b4c3a5acdb720228d6b67817c172deaf39
sakai <sakai263016@gmail.com>
2023-12-20 15:15:47 +0800

    演示字符流写出数据文本

--- src/main/java/io/OSWDemo.java
+++ src/main/java/io/OSWDemo.java
@@ -3,17 +3,20 @@ package io;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 字符流的简单演示与操作
+ * 与WriteStringDemo.java进行对比
  */
 public class OSWDemo {
     public static void main(String[] args) throws IOException {
         FileOutputStream fos = new FileOutputStream("osw.txt");
-        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        //在此处指定字符集
+        OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
 
-        String message = "9527！出列";
-        osw.write(message);
+        osw.write("9527！出列");
+        osw.write("是，9527出列");
         System.out.println("执行完毕");
         osw.close();
     }
