e85f8800218af18a30c0e1f3707e18d914d32005
sakai <sakai263016@gmail.com>
2023-12-21 14:26:26 +0800

    演示异常处理机制中的finally语句块 更新

---
+++ src/main/java/exception/FinallyDemo2.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class FinallyDemo2 {
+    public static void main(String[] args) {
+/*        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream("sakai.txt");
+        }catch (){
+
+        } finally {
+        }
+    }*/
+    }
+}
