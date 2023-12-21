bb3ecf68b194332fb90d9df5f9cfbde79b36a49c
sakai <sakai263016@gmail.com>
2023-12-21 14:27:34 +0800

    演示异常处理机制中的finally语句块 更新

--- src/main/java/exception/FinallyDemo2.java
+++ src/main/java/exception/FinallyDemo2.java
@@ -1,14 +1,28 @@
 package exception;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 异常处理机制在IO中的应用
+ */
 public class FinallyDemo2 {
     public static void main(String[] args) {
-/*        FileInputStream fis = null;
-        try{
-            fis = new FileInputStream("sakai.txt");
-        }catch (){
-
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("fos.dat");
+            fos.write(1);
+        } catch (IOException e) {
+            e.printStackTrace();
         } finally {
+            try {
+                if(fos!=null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }*/
+
     }
 }
