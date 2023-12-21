42a91367c65774eda58941422b9e905f8cabbdd3
sakai <sakai263016@gmail.com>
2023-12-21 11:54:44 +0800

    演示异常处理机制中的finally语句块 更新

--- src/main/java/exception/FinallyDemo.java
+++ src/main/java/exception/FinallyDemo.java
@@ -7,9 +7,11 @@ public class FinallyDemo {
     public static void main(String[] args) {
         System.out.println("开始了");
         try {
-            //String line = "";
-            String line = null;
+            String line = "";
+            //String line = null;
             System.out.println(line.length());
+            //加入return 结束方法 , 如果程序正常，不抛出异常 那么程序不在执行剩余的语句,但是finally语句块的还将继续执行
+            return;
         } catch (Exception e) {
             System.out.println("出错了");
             //finally语句块不论有没有异常都会执行
