55b2f5ed6e3a49e166a15185aa5ba7baa19b329c
sakai <sakai263016@gmail.com>
2023-12-21 11:16:00 +0800

    演示异常处理机制中的try-catch

---
+++ src/main/java/exception/TryCatchDemo.java
@@ -0,0 +1,26 @@
+package exception;
+
+/**
+ *
+ */
+public class TryCatchDemo {
+    public static void main(String[] args) {
+        System.out.println("程序开始执行");
+
+        try {
+            String line = "abc";
+            System.out.println(line.length());
+            System.out.println(line.charAt(1));
+            System.out.println(Integer.parseInt(line));
+        }/*catch (NullPointerException e) {
+            System.out.println("出现了异常，抛出了异常，输出了该句话");
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("出现了下标越界异常，抛出了异常，输出了该句话");
+        }*/ catch (StringIndexOutOfBoundsException | NullPointerException exception) {
+            System.out.println("出现了空指针或下标越界异常");
+        }catch (Exception e ){
+            System.out.println("未知错误");
+        }
+        System.out.println("程序结束");
+    }
+}
