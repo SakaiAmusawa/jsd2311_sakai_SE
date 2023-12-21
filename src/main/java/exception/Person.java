c0e2305bff317f40c8ba9075c0710e438211ed8f
sakai <sakai263016@gmail.com>
2023-12-21 15:06:17 +0800

    throw的演示 更新

--- src/main/java/exception/Person.java
+++ src/main/java/exception/Person.java
@@ -15,10 +15,10 @@ public class Person {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age) throws Exception {
         //使用throw显示的主动抛出异常
         if (age < 0 || age > 100) {
-            throw new RuntimeException("年龄不合法");
+            throw new Exception("年龄不合法");
         }
         this.age = age;
     }
--- src/main/java/exception/ThrowDemo.java
+++ src/main/java/exception/ThrowDemo.java
@@ -3,7 +3,11 @@ package exception;
 public class ThrowDemo {
     public static void main(String[] args) {
         Person person = new Person();
-        person.setAge(1000);
+        try {
+            person.setAge(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("年龄为：" + person.getAge());
     }
 }
