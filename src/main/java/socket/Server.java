bc3f6e277aaedaa2feddb82373b220fcea718cd1
sakai <sakai263016@gmail.com>
2023-12-22 11:43:17 +0800

    简易网络聊天室:客户端发送信息到服务端 更新:允许多个客户端连接,但是无法同时发送信息,需要多线程解决

--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -1,8 +1,12 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
     private ServerSocket serverSocket;
@@ -29,7 +33,6 @@ public class Server {
                 System.out.println("等待客户端连接...");
                 socket = serverSocket.accept();
                 System.out.println("客户端连接完成");
-            }
 
 /*            //进行数据的接受
             InputStream is = socket.getInputStream();
@@ -37,21 +40,22 @@ public class Server {
             int d = is.read();
             System.out.println(d);*/
 
-/*            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-            BufferedReader br = new BufferedReader(isr);
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
 
-            String message;
+                String message;
              /*
                 readLine方法用于读取来自客户端发送过来的一行字符串。
                 该方法有以下几种现象:
                 1:正常返回一行字符串，正常读取
                 2:返回null，说明客户端进行了四次挥手与服务端断开链接，不会再发送数据
-                3:抛出异常，客户端没有进行四次挥手而异常断开
+                3:抛出异常，客户端没有进行四次挥手而异常断开*/
 
-            while ((message = br.readLine()) != null) {
-                System.out.println(message);
-            }*/
+                while ((message = br.readLine()) != null) {
+                    System.out.println(message);
+                }
+            }
             //String message = br.readLine();
             //System.out.println(message);
         } catch (IOException e) {
