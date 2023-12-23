b07db898239caad09344ee7c238d018085a60d53
sakai <sakai263016@gmail.com>
2023-12-23 16:58:37 +0800

    演示了Collections.synchronizedList(new ArrayList<>()) 将原有的集合转变为并发安全的集合

--- src/main/java/thread/concurrent/SyncDemo5.java
+++ src/main/java/thread/concurrent/SyncDemo5.java
@@ -1,6 +1,7 @@
 package thread.concurrent;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 
 /**
@@ -11,7 +12,13 @@ public class SyncDemo5 {
 
         //Collection<Integer> c = new ArrayList<>();
         //Collection<Integer> c = new LinkedList<>();
-        Collection<Integer> c = new HashSet<>();
+        //Collection<Integer> c = new HashSet<>();
+
+        //Collections提供了一个可以将现有集合转变为同步的集合，解决了并发安全的问题
+        //用法如下
+        //Collection<Integer> c = Collections.synchronizedList(new ArrayList<>());
+        //Collection<Integer> c = Collections.synchronizedList(new LinkedList<>());
+        Collection<Integer> c = Collections.synchronizedSet(new HashSet<>());
         Thread thread1 = new Thread() {
             @Override
             public void run() {
