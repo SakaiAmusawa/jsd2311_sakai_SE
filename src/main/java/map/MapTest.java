79ebbb52cf59952707f608600b2c4e409a3a02f3
Sakai <sakai263016@gmail.com>
2023-12-25 16:29:35 +0800

    统计字母出现的次数 更新:新的思路 三目运算

--- src/main/java/map/MapTest.java
+++ src/main/java/map/MapTest.java
@@ -11,14 +11,17 @@ public class MapTest {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < array.length; i++) {
             if (array[i] != ' ') {
-                Integer value = map.get(array[i]);
+/*                Integer value = map.get(array[i]);
                 if (value == null) {
                     map.put(array[i], 1);
                 } else {
                     value = map.get(array[i]);
                     value++;
                     map.put(array[i], value);
-                }
+                }*/
+
+                //使用三目运算更方便
+                map.put(array[i], map.containsKey(array[i]) ? map.get(array[i]) + 1 : 1);
             }
         }
         System.out.println(map);
