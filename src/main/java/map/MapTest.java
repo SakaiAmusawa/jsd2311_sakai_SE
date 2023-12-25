c4db2f7dc3a0eb6fa26b4c0f743e85e44e50defb
Sakai <sakai263016@gmail.com>
2023-12-25 16:08:08 +0800

    统计字母出现的次数

--- src/main/java/map/MapTest.java
+++ src/main/java/map/MapTest.java
@@ -1,4 +1,26 @@
 package map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTest {
+    public static void main(String[] args) {
+        String line = "i love java! thinking in java!! hello world!";
+        char[] array = line.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != ' ') {
+                Integer value = map.get(array[i]);
+                if (value == null) {
+                    map.put(array[i], 1);
+                } else {
+                    value = map.get(array[i]);
+                    value++;
+                    map.put(array[i], value);
+                }
+            }
+        }
+        System.out.println(map);
+    }
 }
